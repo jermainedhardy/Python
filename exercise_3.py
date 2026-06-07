ab978ef0e1d8dffbee5f3178e58fc2220a6fbcfc
Jermaine Hardy <jermainedhardy3@gmail.com>
2026-06-06 21:28:22 -0500

    Create exercise_3.py.

---
+++ exercise_3.py
@@ -0,0 +1,7 @@
+favorite_movie = "The Magician"
+favorite_game = "Gears of War"
+favorite_number = 3
+
+print("My favorite movie is " + favorite_movie + ".")
+print("My favorite game is " + favorite_game + ".")
+print("My favorite number is " + str(favorite_number) + ".")
