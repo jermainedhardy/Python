cddcdd1df81b45ae91f8f9ca2038083b38cc0edc
Jermaine Hardy <jermainedhardy3@gmail.com>
2026-06-08 23:48:28 -0500

    June 8th practice.

---
+++ python_crash_course/chapter_2_variables_and_simple_data_types/exercises/exercise_2_8.py
@@ -0,0 +1,6 @@
+# Program to calculate 8 different ways
+
+print(3 + 5)
+print(10 - 2)
+print(2 * 4)
+print(24 / 3)
---
+++ python_crash_course/chapter_2_variables_and_simple_data_types/exercises/exercise_2_9.py
@@ -0,0 +1,4 @@
+# Program to share my favorite number
+favorite_number = 3
+
+print("My favorite number is " + str(favorite_number))
